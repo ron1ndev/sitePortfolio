a94714165dbec1532b72511bfdf6d22724e5bd9b
Роман Громов <roma-gromov-1990@mail.ru>
2024-11-10 00:44:13 +0500

    Layout of header, footer, hero

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,37 @@
-console.log(3)
+//
 
-console.log(2)
+window.addEventListener('load',()=>{
+    heroTitleOverlay1 = document.querySelector('.hero__title-overlay1');
+    heroTitleOverlay2 = document.querySelector('.hero__title-overlay2');
+    setTimeout(()=>{
+        heroTitleOverlay1.classList.add('active');
+    },2000)
+    
+    setTimeout(()=>{
+        heroTitleOverlay2.classList.add('active');
+    },500)
+   
+
+})
+const helloText = document.querySelector('.hello__text');
+let text = 'Hello,world'
+let index = 0;
+
+
+
+
+function printText(){
+    
+    helloText.innerHTML = text.slice(0,index);
+    index++
+
+    if(index<=text.length){
+        setTimeout(()=>{
+            printText()
+        },200)
+    }
+}
+
+
+
+printText()
