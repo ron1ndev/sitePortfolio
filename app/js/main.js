c8d9405bfbad9b9bd1097fd1654117997ef33959
Роман Громов <roma-gromov-1990@mail.ru>
2024-11-10 22:57:43 +0500

    Layout of projects

--- app/js/main.js
+++ app/js/main.js
@@ -1,37 +1,140 @@
-//
-
+// Появление элементов на странице
 window.addEventListener('load',()=>{
-    heroTitleOverlay1 = document.querySelector('.hero__title-overlay1');
-    heroTitleOverlay2 = document.querySelector('.hero__title-overlay2');
-    setTimeout(()=>{
-        heroTitleOverlay1.classList.add('active');
-    },2000)
+
+    // функция для показа элемента 
+    function showElem(selector,speed,clazz='active'){
+
+        setTimeout(()=>{
+            selector.classList.add(clazz);
+        },speed)
+    }
     
-    setTimeout(()=>{
-        heroTitleOverlay2.classList.add('active');
-    },500)
-   
+    showElem(document.querySelector('.hero__title-overlay1'),1000)
+    showElem(document.querySelector('.hero__title-overlay2'),500)
+
+
+    // Автоматическое печатание текста на странице
+    const helloText = document.querySelector('.hello__text');
+    const heroTitleDev= document.querySelector('.hero__title-dev');
+
+    function printText(selector,text,index,speed){
+
+        selector.innerHTML = text.slice(0,index);
+        index++
+
+        if(index <= text.length){
+            setTimeout(()=>printText(selector,text,index,speed),speed)
+        }
+    }
+
+    printText(helloText,'Hello,world',0,100)
+    printText(heroTitleDev,'Разработка',0,170)
 
 })
-const helloText = document.querySelector('.hello__text');
-let text = 'Hello,world'
-let index = 0;
 
 
 
+// Tabs
 
-function printText(){
-    
-    helloText.innerHTML = text.slice(0,index);
-    index++
+const tabsContols = document.querySelectorAll('.contols-tabs__item');
+const slides = document.querySelectorAll('.swiper-slide');
 
-    if(index<=text.length){
-        setTimeout(()=>{
-            printText()
-        },200)
+
+tabsContols.forEach((item,index)=>{
+item.addEventListener('click',(e)=>{
+
+    swiper.slideTo(0);
+
+    slides.forEach((item)=>{
+        item.classList.remove('active')
+    })
+    tabsContols.forEach((item)=>{
+        item.classList.remove('active')
+    })
+    e.target.classList.add('active')
+
+    if(index===0){
+        slides.forEach((item)=>{
+            item.classList.add('active')
+        })
     }
-}
 
+    if(index===1){
+        slides.forEach((item)=>{
+            if(item.getAttribute('data-info')==='internet'){
+                item.classList.add('active')
+            }
+        })
+    }
 
+    if(index===2){
+        slides.forEach((item)=>{
+            if(item.getAttribute('data-info')==='lending'){
+                item.classList.add('active')
+            }
+        })
+    }
 
-printText()
+    if(index===3){
+        slides.forEach((item)=>{
+            if(item.getAttribute('data-info')==='letter'){
+                item.classList.add('active')
+            }
+        })
+    }
+
+})
+})
+
+// Swiper
+
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'horizontal',
+//     loop: true,
+  
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+  
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+  
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+//   });
+
+  var swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 50,
+      },
+    },
+
+        navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
